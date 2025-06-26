f4c5a65d0edc825678f35e5dc40599c6522f51f5
linuxtaolinran <linuxtaolinran@gmail.com>
2025-06-26 04:14:20 +0000

    with preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://colintao.netlify.app/"
+  site: "https://colintao.netlify.app/",
+  integrations: [preact()]
 });
